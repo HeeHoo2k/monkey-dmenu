9f646428d535fa497ea91b3e2b2c68cf9750445f
HeeHoo2k <135641737+HeeHoo2k@users.noreply.github.com>
2023-06-06 09:07:18 -0600

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -117,7 +117,7 @@ char *colors[][2] = {
 static unsigned int lines      = 0;
 #if GRID_PATCH
 /* -g option; if nonzero, dmenu uses a grid comprised of columns and lines */
-static unsigned int columns    = 0;
+static unsigned int columns    = 1;
 #endif // GRID_PATCH
 #if LINE_HEIGHT_PATCH
 static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
